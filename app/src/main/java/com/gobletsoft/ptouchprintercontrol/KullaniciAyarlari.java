e22e5a9dadcc65d7ffb3db0c0a530043a2f6b893
Ahmet Kılınç <ahmetkilinc3899@gmail.com>
2018-06-18 09:00:38 +0300

    +Kullanıcı Ayarları-Güncelleme fonksiyonu.

--- app/src/main/java/com/gobletsoft/ptouchprintercontrol/KullaniciAyarlari.java
+++ app/src/main/java/com/gobletsoft/ptouchprintercontrol/KullaniciAyarlari.java
@@ -1,11 +1,16 @@
 package com.gobletsoft.ptouchprintercontrol;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,24 +26,44 @@ import java.util.List;
 public class KullaniciAyarlari extends AppCompatActivity {
 
     SessionManager session;
-
+    //1.0
     private ProgressDialog pDialog;
 
     //php connections
     JSONParser jsonParser = new JSONParser();
-    private static String url_login = "http://10.0.0.100:85/ptouchAndroid/hesapbilgilerinial.php";
+    private static String url_hesapbilgilerini_getir = "http://10.0.0.100:85/ptouchAndroid/hesapbilgilerinial.php";
+    private static String url_hesapbilgilerini_guncelle = "http://10.0.0.100:85/ptouchAndroid/hesapbilgileriniguncelle.php";
     private static final String TAG_SUCCESS = "success";
     private JSONObject json;
+    //1.1
 
+    //1.2
     private String email;
-    private TextView tvEmail;
+    private EditText etKullaniciAdi;
+    private EditText etAd;
+    private EditText etSoyad;
+    private EditText etEposta;
+    private EditText etSifre;
+    private Button btnKullaniciAyarlariGuncelle;
 
+    private String GuncelKullaniciAdi;
+    private String GuncelAd;
+    private String GuncelSoyad;
+    private String GuncelEmail;
+    private String GuncelSifre;
+
+    //1.3
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_kullanici_ayarlari);
 
-        tvEmail = findViewById(R.id.textViewEmail);
 
         session = new SessionManager(getApplicationContext());
 
@@ -46,12 +71,47 @@ public class KullaniciAyarlari extends AppCompatActivity {
 
         HashMap<String, String> user = session.getUserDetails();
 
+        etKullaniciAdi = findViewById(R.id.editTextKullaniciAdi);
+        etAd = findViewById(R.id.editTextAd);
+        etSoyad = findViewById(R.id.editTextSoyad);
+        etEposta = findViewById(R.id.editTextEmailKa);
+        etSifre = findViewById(R.id.editTextSifre);
+        btnKullaniciAyarlariGuncelle = findViewById(R.id.buttonKullaniciAyarlariGuncelle);
+
+        etEposta.setFocusable(false);
+
+
+
+        //1.4
         email = user.get(SessionManager.KEY_NAME);
         String sifre = user.get(SessionManager.KEY_EMAIL);
 
         //tvEmail.setText(email + sifre);
 
         new kullaniciBilgileriniGetir().execute();
+
+        btnKullaniciAyarlariGuncelle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (etKullaniciAdi.getText().toString().isEmpty() || etAd.getText().toString().isEmpty() || etSoyad.getText().toString().isEmpty() ||
+                        etEposta.getText().toString().isEmpty() || etSifre.getText().toString().isEmpty()){
+
+                    Toast.makeText(getApplicationContext(), "Lütfen Tüm Boşlukları Doldurunuz.", Toast.LENGTH_LONG);
+                }
+
+                else{
+
+                    GuncelKullaniciAdi = etKullaniciAdi.getText().toString();
+                    GuncelAd = etAd.getText().toString();
+                    GuncelSoyad = etSoyad.getText().toString();
+                    GuncelEmail = etEposta.getText().toString();
+                    GuncelSifre = etSifre.getText().toString();
+
+                    new kullaniciBilgileriniGuncelle().execute();
+                }
+            }
+        });
     }
 
     class kullaniciBilgileriniGetir extends AsyncTask<String,String,String> {
@@ -61,7 +121,7 @@ public class KullaniciAyarlari extends AppCompatActivity {
 
             super.onPreExecute();
             pDialog = new ProgressDialog(KullaniciAyarlari.this);
-            pDialog.setMessage("Giriş Yapılıyor");
+            pDialog.setMessage("Kullanıcı Bilgileri Getiriliyor...");
             pDialog.setIndeterminate(false);
             pDialog.setCancelable(true);
             pDialog.show();
@@ -74,7 +134,7 @@ public class KullaniciAyarlari extends AppCompatActivity {
 
             params.add(new BasicNameValuePair("kullanici_email", email));
 
-            json = jsonParser.makeHttpRequest(url_login,
+            json = jsonParser.makeHttpRequest(url_hesapbilgilerini_getir,
                     "POST", params);
 
             // check log cat for response
@@ -89,11 +149,17 @@ public class KullaniciAyarlari extends AppCompatActivity {
 
             try {
 
-                String kullaniciAdi = json.getString("kullaniciadi");
+                String kullaniciAdi = json.getString("kullaniciad");
+                String ad = json.getString("ad");
+                String soyad = json.getString("soyad");
+                String email = json.getString("email");
+                String sifre = json.getString("sifre");
 
-                Toast.makeText(getApplicationContext(), kullaniciAdi, Toast.LENGTH_LONG).show();
-                //tvEmail.setText(kullaniciAdi);
-
+                etKullaniciAdi.setText(kullaniciAdi);
+                etAd.setText(ad);
+                etSoyad.setText(soyad);
+                etEposta.setText(email);
+                etSifre.setText(sifre);
             }
             catch (JSONException e) {
 
@@ -101,4 +167,65 @@ public class KullaniciAyarlari extends AppCompatActivity {
             }
         }
     }
+
+    class kullaniciBilgileriniGuncelle extends AsyncTask<String,String,String> {
+
+        @Override
+        protected void onPreExecute() {
+
+            super.onPreExecute();
+            pDialog = new ProgressDialog(KullaniciAyarlari.this);
+            pDialog.setMessage("Bilgileriniz Güncelleniyor...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        protected String doInBackground(String... args){
+
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<>();
+
+            params.add(new BasicNameValuePair("email", GuncelEmail));
+            params.add(new BasicNameValuePair("kullanici_adi", GuncelKullaniciAdi));
+            params.add(new BasicNameValuePair("ad", GuncelAd));
+            params.add(new BasicNameValuePair("soyad", GuncelSoyad));
+            params.add(new BasicNameValuePair("sifre", GuncelSifre));
+
+            json = jsonParser.makeHttpRequest(url_hesapbilgilerini_guncelle,
+                    "POST", params);
+
+            // check log cat for response
+            Log.d("Create Response", json.toString());
+
+            return null;
+        }
+
+        protected void onPostExecute(String file_url){
+
+            pDialog.dismiss();
+
+            try {
+
+                int success = json.getInt("success");
+
+                if (success == 1){
+
+                    Toast.makeText(getApplicationContext(), "Bilgileriniz Başarı ile Güncellendi.", Toast.LENGTH_LONG).show();
+                }
+
+                else{
+
+                    Toast.makeText(getApplicationContext(), "Verilen Bilgilerde Bir Sıkıntı Oldu, Lütfen Tekrar Deneyin.",Toast.LENGTH_LONG);
+                }
+            }
+            catch (JSONException e) {
+
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
 }
